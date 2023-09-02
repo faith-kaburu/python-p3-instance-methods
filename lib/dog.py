6d3b69a5f57b5d8f01ee240d95856247f6a4c665
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-09-02 18:09:35 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,20 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof")
+    #New instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+    
+fido = Dog()  # Create an instance of the Dog class
+fido.bark()   # Call the bark method to produce the output
+fido.sit()    #Call the sit method to produce the output
+
+snoopy = Dog()
+snoopy.bark() 
+snoopy.sit()  #Call the sit method on the snoopy instance
+
+
+    
